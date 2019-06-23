a35efe909f8b49e3eaf3449e6350938ab7c1e7f9
Lola Barbier <lolabarbierhoffenbach@gmail.com>
2019-06-23 13:05:39 +0200

    can't figure out shuffle words

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="container">
     <Marquee/>
-    <div id="about-me">
-      <b>hi i'm <u>l o l a</u></b>
-      <br>
-      <br>
-      i like many things but i make my living as a web developer
-      <br>
-      <br>
-      i live in paris and am not super sure what to put in this space yet
-    </div>
+    <AboutMe/>
     <PrizeButton/>
   </div>
 </template>
 
 <script>
-import Marquee from './components/Marquee.vue'
-import PrizeButton from './components/PrizeButton.vue'
+import Marquee from './components/Marquee.vue';
+import AboutMe from './components/AboutMe.vue';
+import PrizeButton from './components/PrizeButton.vue';
 
 
 export default {
   name: 'Homepage',
-  components: { Marquee, PrizeButton }
+  components: { Marquee, AboutMe, PrizeButton }
 }
 </script>
 
@@ -37,16 +30,5 @@ body {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-#about-me {
-  background: white;
-  opacity: .6;
-  width: 200px;
-  font-family: monospace, monospace;
-  font-size: 12px;
-  border: 5px dashed orange;
-  outline: 5px dotted blue;
-  padding: 20px;
 }
 </style>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="about-me">
+    <b>hi i'm <u>lola</u></b>
+    <br>
+    <br>
+    i like many things but i make my living as a web developer
+    <br>
+    <br>
+    i live in paris and am not super sure what to put in this space yet
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'; 
+
+export default {
+  name: 'AboutMe',
+  components: {},
+}
+</script>
+
+<style lang="scss">
+#about-me {
+  background: white;
+  opacity: .6;
+  width: 200px;
+  font-family: monospace, monospace;
+  font-size: 12px;
+  border: 5px dashed orange;
+  outline: 5px dotted blue;
+  padding: 20px;
+}
+</style>
